e9705d613eff19781dd2f7bd0e2728709183b4b0
Maksim Kozlovsky <jstdntcr.m@gmail.com>
2025-03-31 22:08:58 +0300

    Simple express app created

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,12 @@
-console.log('Hello!');
+import express from "express";
+
+const expressApp = express();
+const PORT = 8080;
+
+expressApp.get("/ping", (req, res) => {
+    res.status(200).send("pong");
+});
+
+expressApp.listen(PORT, ()=> {
+    console.log(`Server is running on port http://localhost:${PORT}`);
+})
