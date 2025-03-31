e780bc3955fcd5ee80b39a51967e0240c62bc90c
Maksim Kozlovsky <jstdntcr.m@gmail.com>
2025-03-31 22:13:54 +0300

    New endpoint '/reviews' was added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,38 @@
 import express from "express";
 
+const reviews = [
+    {
+      nick: 'cool-review-1',
+      name: 'review 1',
+      description: 'review 1 description',
+    },
+    {
+      nick: 'cool-review-2',
+      name: 'review 2',
+      description: 'review 2 description',
+    },
+    {
+      nick: 'cool-review-3',
+      name: 'review 3',
+      description: 'review 3 description',
+    },
+    {
+      nick: 'cool-review-4',
+      name: 'review 4',
+      description: 'review 4 description',
+    },
+    {
+      nick: 'cool-review-5',
+      name: 'review 5',
+      description: 'review 5 description',
+    },
+  ];
+
 const expressApp = express();
 const PORT = 8080;
 
-expressApp.get("/ping", (req, res) => {
-    res.status(200).send("pong");
+expressApp.get("/reviews", (req, res) => {
+    res.status(200).send(reviews);
 });
 
 expressApp.listen(PORT, ()=> {
