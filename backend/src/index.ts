1f107ada711d37592430b5ee7ed73aefa04afd4f
Maksim Kozlovsky <jstdntcr.m@gmail.com>
2025-04-03 22:51:43 +0300

    tRPC fro backend added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,40 +1,16 @@
-import express from "express";
-
-const reviews = [
-    {
-      nick: 'cool-review-1',
-      name: 'review 1',
-      description: 'review 1 description',
-    },
-    {
-      nick: 'cool-review-2',
-      name: 'review 2',
-      description: 'review 2 description',
-    },
-    {
-      nick: 'cool-review-3',
-      name: 'review 3',
-      description: 'review 3 description',
-    },
-    {
-      nick: 'cool-review-4',
-      name: 'review 4',
-      description: 'review 4 description',
-    },
-    {
-      nick: 'cool-review-5',
-      name: 'review 5',
-      description: 'review 5 description',
-    },
-  ];
+import express from 'express';
+import * as trpcExpress from '@trpc/server/adapters/express';
+import { routerTRPC } from './trpc';
 
 const expressApp = express();
 const PORT = 8080;
 
-expressApp.get("/reviews", (req, res) => {
-    res.status(200).send(reviews);
-});
+expressApp.use('/trpc', trpcExpress.createExpressMiddleware(
+  {
+    router: routerTRPC
+  }
+))
 
-expressApp.listen(PORT, ()=> {
-    console.log(`Server is running on port http://localhost:${PORT}`);
-})
+expressApp.listen(PORT, () => {
+  console.log(`Server is running on port http://localhost:${PORT}`);
+});
