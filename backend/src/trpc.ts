5d316fdf492f90b0dafd79cb9e7b9e79748044d2
Maksim Kozlovsky <jstdntcr.m@gmail.com>
2025-04-25 21:14:40 +0300

    Creating a tRPC procedure with input parameters

--- backend/src/trpc.ts
+++ backend/src/trpc.ts
@@ -1,11 +1,12 @@
 import { initTRPC } from '@trpc/server';
 import _ from 'lodash';
+import z from 'zod';
 
 const reviews = _.times(100, (i) => ({
   nick: `cool-review-${i}`,
   name: `review ${i}`,
   description: `review ${i} description`,
-  text: _.times(100, (j) => `<p>Text paragraph of review ${i} and paragraph ${j}</p>`).join(''),
+  text: _.times(100, (j) => `<p>Text paragraph ${j} of review ${i}</p>`).join(''),
 }));
 
 const trpc = initTRPC.create();
@@ -14,6 +15,16 @@ export const routerTRPC = trpc.router({
   getReviews: trpc.procedure.query(() => {
     return { reviews: reviews.map((review) => _.pick(review, ['nick', 'name', 'description'])) };
   }),
+  getReview: trpc.procedure
+    .input(
+      z.object({
+        reviewNick: z.string(),
+      })
+    )
+    .query(({ input }) => {
+      const review = reviews.find((review) => review.nick === input.reviewNick);
+      return { review: review || null };
+    }),
 });
 
 export type AppRouter = typeof routerTRPC;
--- webapp/src/pages/ViewReviewPage/index.tsx
+++ webapp/src/pages/ViewReviewPage/index.tsx
@@ -1,18 +1,21 @@
 import { useParams } from 'react-router-dom';
 import { ViewReviewRouteParams } from '../../lib/routes';
+import { trpc } from '../../lib/trpc';
 
 export const ViewReviewPage = () => {
   const { reviewNick } = useParams() as ViewReviewRouteParams;
 
+  const { data, error, isLoading, isFetching, isError } = trpc.getReview.useQuery({ reviewNick });
+
+  if (isLoading || isFetching) return <span>Loading...</span>;
+  if (isError) return <span>Error: {error.message}</span>;
+  if (!data?.review) return <span>Idea not found</span>;
+
   return (
     <div>
-      <h1>{reviewNick}</h1>
-      <p>{reviewNick} description</p>
-      <div>
-        <p>Paragraph 1 of {reviewNick}</p>
-        <p>Paragraph 2 of {reviewNick}</p>
-        <p>Paragraph 3 of {reviewNick}</p>
-      </div>
+      <h1>{data.review.name}</h1>
+      <p>{data.review.description}</p>
+      <div dangerouslySetInnerHTML={{ __html: data.review.text }} />
     </div>
   );
 };
