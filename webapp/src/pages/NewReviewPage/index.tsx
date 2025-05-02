ce3158f16d987e86c10c17af2e9c52b1402f3c21
Maksim Kozlovsky <jstdntcr.m@gmail.com>
2025-05-02 21:39:11 +0300

    Form validation

--- webapp/src/pages/NewReviewPage/index.tsx
+++ webapp/src/pages/NewReviewPage/index.tsx
@@ -11,6 +11,26 @@ export const NewReviewPage = () => {
       description: '',
       text: '',
     },
+    validate: (values) => {
+      const errors: Partial<typeof values> = {};
+      if (!values.name) {
+        errors.name = 'Name is required';
+      }
+      if (!values.nick) {
+        errors.nick = 'Nick is required';
+      } else if (!values.nick.match(/^[a-z0-9-]+$/)) {
+        errors.nick = 'Nick can only contain lowercase letters, numbers and dashes';
+      }
+      if (!values.description) {
+        errors.description = 'Description is required';
+      }
+      if (!values.text) {
+        errors.text = 'Text is required';
+      } else if (values.text.length < 100) {
+        errors.text = 'Text must be at least 100 characters';
+      }
+      return errors;
+    },
     onSubmit: (values) => {
       console.info('Submitted', values);
     },
