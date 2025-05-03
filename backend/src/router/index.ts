324e6c5f39982aad4659926726b1fe807e9d15dc
Maksim Kozlovsky <jstdntcr.m@gmail.com>
2025-05-03 23:38:31 +0300

    Automatic file index generation in backend

--- backend/src/router/index.ts
+++ backend/src/router/index.ts
@@ -1,10 +1,14 @@
+// @index('./**/index.ts', f => `import { ${f.path.split('/').slice(0, -1).pop()}TrpcRoute } from '${f.path.split('/').slice(0, -1).join('/')}'`)
 import { getReviewTrpcRoute } from './getReview';
 import { getReviewsTrpcRoute } from './getReviews';
+// @endindex
 import { trpc } from '../lib/trpc';
 
 export const trpcRouter = trpc.router({
-  getReviews: getReviewsTrpcRoute,
+  // @index('./**/index.ts', f => `${f.path.split('/').slice(0, -1).pop()}: ${f.path.split('/').slice(0, -1).pop()}TrpcRoute,`)
   getReview: getReviewTrpcRoute,
+  getReviews: getReviewsTrpcRoute,
+  // @endindex
 });
 
 export type TrpcRouter = typeof trpcRouter;
