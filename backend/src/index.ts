d73838f5c8e918c62025f4b5587aca70e69029db
Maksim Kozlovsky <jstdntcr.m@gmail.com>
2025-05-05 22:10:04 +0300

    Use Prisma in tRPC context

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,24 @@
 import cors from 'cors';
 import express from 'express';
+import { createAppContext } from './lib/ctx';
+import { AppContext } from './lib/ctx';
 import { applyTrpcToExpressApp } from './lib/trpc';
 import { trpcRouter } from './router';
 
-const expressApp = express();
-const PORT = 3000;
+void (async () => {
+  let ctx: AppContext | null = null;
+  try {
+    ctx = createAppContext();
+    const expressApp = express();
+    const PORT = 3000;
 
-expressApp.use(cors());
-
-applyTrpcToExpressApp(expressApp, trpcRouter);
-
-expressApp.listen(PORT, () => {
-  console.info(`Server is running on port http://localhost:${PORT}`);
-});
+    expressApp.use(cors());
+    applyTrpcToExpressApp(expressApp, ctx, trpcRouter);
+    expressApp.listen(PORT, () => {
+      console.info(`Server is running on port http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error(error);
+    await ctx?.stop();
+  }
+})();
--- backend/src/router/getReviews/index.ts
+++ backend/src/router/getReviews/index.ts
@@ -1,7 +1,14 @@
-import _ from 'lodash';
-import { reviews } from '../../lib/reviews';
 import { trpc } from '../../lib/trpc';
 
-export const getReviewsTrpcRoute = trpc.procedure.query(() => {
-  return { reviews: reviews.map((review) => _.pick(review, ['nick', 'name', 'description'])) };
+export const getReviewsTrpcRoute = trpc.procedure.query(async ({ ctx }) => {
+  const reviews = await ctx.prisma.review.findMany({
+    select: {
+      id: true,
+      nick: true,
+      name: true,
+      description: true,
+    },
+  });
+
+  return { reviews };
 });
