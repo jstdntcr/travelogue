8ef1ef0b69771947fcf444a3659a4897e1f8d456
Maksim Kozlovsky <jstdntcr.m@gmail.com>
2025-05-27 18:37:27 +0300

    Bug fix: access to AddReview page have authorized users only

--- webapp/src/pages/NewReviewPage/index.tsx
+++ webapp/src/pages/NewReviewPage/index.tsx
@@ -2,15 +2,20 @@ import { zCreateReviewTrpcInput } from '@travelogue/backend/src/router/createRev
 import { useFormik } from 'formik';
 import { withZodSchema } from 'formik-validator-zod';
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Alert } from '../../components/Alert';
 import { Button } from '../../components/Button';
 import { FormItems } from '../../components/FormItems';
 import { Input } from '../../components/Input';
 import { Segment } from '../../components/Segment';
 import { Textarea } from '../../components/Textarea';
+import { getAllReviewsRoute } from '../../lib/routes';
 import { trpc } from '../../lib/trpc';
 
 export const NewReviewPage = () => {
+  const { data } = trpc.getMe.useQuery();
+  const trpcUtils = trpc.useUtils();
+  const navigate = useNavigate();
   const [successMessageVisible, setSuccessMessageVisibe] = useState(false);
   const [submittingError, setSubmittingError] = useState<string | null>(null);
   const createReview = trpc.createReview.useMutation();
@@ -38,6 +43,11 @@ export const NewReviewPage = () => {
       }
     },
   });
+
+  void trpcUtils.invalidate();
+  if (!data?.me) {
+    navigate(getAllReviewsRoute());
+  }
 
   return (
     <Segment title="New Review">
